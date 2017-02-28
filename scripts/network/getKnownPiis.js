ac05e20b1efba55dc3fb80cc80088e43115f4a9f
Thy <thierry@ovh1>
2017-02-28 16:57:58 +0100

    first commit

--- scripts/network/getKnownPiis.js
+++ scripts/network/getKnownPiis.js
@@ -1,18 +1,37 @@
 var fs = require ('fs')
 
 var undef
+var dir = process.argv [2]
+var piiDic = {}
 
-files = fs.readdirSync ('./HL/')
+function outputPiis (xs) {
+    var x = xs.match (/(S(?:X|\d){16})/g)
+    if (x !== null)
+	x.forEach (
+	    function (x) {
+		if (piiDic [x] === undef) {
+		    piiDic [x] = 1
+		    process.stdout.write (x + '\n')
+		}
+	    })	    
+}
+
+files = fs.readdirSync (dir)
 files.forEach (function (f) {
-    var fn = 'HL/' + f
-    try {
-	fs.readFile (fn 
-	x.match (/(S(?:X|\d){16})/g).forEach (function (p) {
-	    process.stdout.write (p + '\n')
-	})}
-    catch (err) {
-	process.stderr.write ('Unable to read file ' + fn)
-	process.exit ()
-    }
+    var fn = dir + '/' + f
+    var s = fs.createReadStream (fn, {encoding:'utf8'})
+    var data = ''
+    s.on ('data', function (d) {
+	data = data + d
+	var lines = data.split (/\r?\n/)
+	data = lines.pop ()
+	lines.forEach (function (l) {
+	    outputPiis (l)
+	})
+    })
+    s.on ('end', function () {
+	outputPiis (data)
+    })
 })
+
 
