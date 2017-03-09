9373b0098b1896408078c2eb653d8e0330101efb
Thy <thierry@ovh1>
2017-03-09 18:48:46 +0100

    HL dir moved

--- scripts/network/do.sh
+++ scripts/network/do.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-DIR=/home/thierry/HL/scripts/network
+WORKDIR=/home/thierry/HL
+DIR=$WORKDIR/scripts/network
+HLDIR=$WORKDIR/data/in/HL
+
 cd $DIR
 TIMESTAMP=`date +'%s'`
 UNKNOWN=UNKNOWN/$TIMESTAMP
+NODE=/usr/local/n/versions/node/7.6.0/bin/node
 
-node runGetPiis.js 2>LOGS/getpiis.$TIMESTAMP
-node getUnknownPiis.js HL PIIS/ > $UNKNOWN
-node runGetHLPar.js $UNKNOWN > HL/HL.$TIMESTAMP 2> LOGS/log.$TIMESTAMP
+grep reques LOGS/* | perl -ne '/(S(?:X|\d){16})\s/ and print "$1\n"' | sort -u > $HLDIR/pbaccess
+$NODE runGetPiis.js 2>LOGS/getpiis.$TIMESTAMP
+$NODE getUnknownPiis.js $HLDIR PIIS/ > $UNKNOWN
+$NODE runGetHLPar.js $UNKNOWN > $HLDIR/HL.$TIMESTAMP 2> LOGS/log.$TIMESTAMP
