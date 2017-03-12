91bb011c336a136b89f4e93dee71557db43411cb
Thy <thierry@ovh1>
2017-03-12 12:31:01 +0100

    make a module and load in a dictionary

--- scripts/fingerprint/digest.js
+++ scripts/fingerprint/digest.js
@@ -14,12 +14,17 @@ const extractPiiFromPath = path => {
     const m = path.match (/(S(?:X|\d){16})/)
     return m && m [1] }
 
-const extractForPii = (pii, doc) => 
-    xpath.select ("//*[local-name(.)='Annotation']", doc).map (
-	((e, i) => `${pii}\t${i+1}\t${Features.map (f => getFeature (f, e)).join ('\t')}`)
-    ).join ('\n')
+const extractForPii = (pii, doc, features=Features) => 
+      xpath.select ("//*[local-name(.)='Annotation']", doc).map (
+	  ((e, i) => `${pii}\t${i+1}\t${features.map (f => getFeature (f, e)).join ('\t')}`)
+      ).join ('\n')
 
-const extractTermsForFile = file => new Promise ((resolve, reject) => {
+const extractObjectsForPii = (pii, doc, features=Features) =>
+      ({pii: pii,
+	values: xpath.select ("//*[local-name(.)='Annotation']", doc).map (
+	    (e, i) => features.reduce ((a, f) => Object.assign (a, {[f]: getFeature (f, e)}),{}))})
+
+const extractTermsForFile = (file, extractor=extractForPii) => new Promise ((resolve, reject) => {
     const pii = extractPiiFromPath (file)
     if (! pii) reject (`Cannot extract pii from ${file}`)
     try { fs.readFile (file,
@@ -27,7 +32,7 @@ const extractTermsForFile = file => new Promise ((resolve, reject) => {
 		       (err, data) => {
 			   try { if (err) throw (err)
 				 const doc = new xmldom.DOMParser ().parseFromString (data)
-				 resolve (extractForPii (pii, doc))}
+				 resolve (extractor (pii, doc))}
 			   catch (err) {reject (err) }})
 	} catch (err) {reject (e)}})
 
@@ -35,7 +40,7 @@ const flatten = xs => xs.reduce ((a, b) => {
     if (b === undefined) return a
     return a.concat (Array.isArray (b) ? flatten (b) : [b])}, [])
 
-const browseDirectory = d => new Promise ((resolve, reject) => {
+const browseDirectory = (d, extractor=extractForPii) => new Promise ((resolve, reject) => {
     try { fs.readdir (d,
 		      {encoding: 'utf8'},
 		      (err, xs) => {
@@ -44,9 +49,9 @@ const browseDirectory = d => new Promise ((resolve, reject) => {
 				    const fullname = `${d}/${name}`
 				    const stat = fs.lstatSync (fullname)
 				    if (! stat.isDirectory() && stat.isFile () && extractPiiFromPath (name))
-					return extractTermsForFile (fullname)
+					return extractTermsForFile (fullname, extractor)
 				    else if (stat.isDirectory ()) {
-					return browseDirectory (fullname) }}))}
+					return browseDirectory (fullname, extractor).then (ps => Promise.all (ps)) }}))}
 			  catch (err) {reject (err)}})}
     catch (err) {reject (err)}})
 
@@ -60,7 +65,34 @@ const digest = fun =>
 		 x ['then'] (digest (fun)) ['catch'] (whenErr)
 	     else fun (x) }
 
-browseDirectory (process.argv [2] || '/home/thierry/HL/data/FP').
-    then (digest (whenString)).
-    catch (whenErr)
+
+/*
+browseDirectory (process.argv [2] || '/home/thierry/HL/data/FP', extractObjectsForPii).
+    then (ps => Promise.all (ps)).
+    then (xs => flatten (xs).forEach (x => {
+	const pii = x.pii
+	if (pii) {
+	    const e = dict [pii]
+	    if (! e) dict [pii] = x.values
+	    else e.push (x.values)
+	}
+    })).
+    then (() => console.log (js.stringify (dict))).
+    ['catch'] (whenErr)
+*/
+
+module.exports = function () {
+    const myself = this
+    this.dict = {}
+    
+    this.load = (path='/home/thierry/HL/data/FP') =>
+	  browseDirectory (path, extractObjectsForPii).
+	  then (ps => Promise.all (ps)).
+	  then (xs => flatten (xs).forEach (x => {
+	      const pii = x.pii
+	      if (pii) { const e = myself.dict [pii]
+			 if (! e) myself.dict [pii] = x.values
+			 else e.push (x.values)
+		       } }))
+}
 
