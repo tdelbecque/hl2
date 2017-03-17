db7d6c992d7f621d17f21bf96472d5b135e481da
Thy <thierry@ovh1>
2017-03-17 18:29:38 +0100

    change Set to Map

--- scripts/ontology/compareFPWithOmniscience.js
+++ scripts/ontology/compareFPWithOmniscience.js
@@ -7,11 +7,13 @@ var g = new finger ()
 Promise.all ([f.load (), g.load ()]).then (
     xs => {
 	const concepts = new Set ()
-	const FPConcepts = new Set ()
+	const FPConcepts = new Map ()
 	f.data.forEach (x => concepts.add (x ['Key UID']))
 	Object.keys (g.dict).forEach (
-	    x => g.dict [x].forEach (x => FPConcepts.add (x ['ConceptID'])))
-	FPConcepts.forEach (x => console.log (`${x}\t${concepts.has(x) ? 'FOUND' : 'NOT'}`))
+	    x => g.dict [x].forEach (x => FPConcepts.set (x.ConceptID, `(${x.TermType})\t(${x.Text})\t(${x.Thesaurus})`)))
+	for (let  [k, v] of FPConcepts.entries ()) console.log (`${k}\t${v}\t${concepts.has(k) ? 'FOUND' : 'NOT'}`)
 			    
     }
-)
+).catch (err => console.error (err))
+
+    
