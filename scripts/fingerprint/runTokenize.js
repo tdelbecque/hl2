11018b29aef60cc5e74160aa08947ac9a7e051ab
Thy <thierry@ovh1>
2017-03-31 17:43:39 +0200

    set FP directory

--- scripts/fingerprint/runTokenize.js
+++ scripts/fingerprint/runTokenize.js
@@ -4,10 +4,11 @@ if (!process.argv [2]) {
     u.croak ('Missing argument: file to tokenize')
     process.exit (-1)
 }
+const FPDirectory = process.argv [3]
 
 const hlfile = process.argv [2]
 const m = require ('./tokenize')
-const f = new m ()
+const f = new m (FPDirectory)
 
 f.load (hlfile).
     then (() =>
