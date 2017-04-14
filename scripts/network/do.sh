f72fa5d47538abb16f31eaac8d07232099d1d388
Thy <thierry@ovh1>
2017-04-14 16:05:16 +0200

    Chain make.sh

--- scripts/network/do.sh
+++ scripts/network/do.sh
@@ -2,7 +2,10 @@
 
 WORKDIR=/home/thierry/HL
 DIR=$WORKDIR/scripts/network
-HLDIR=$WORKDIR/data/in/HL
+DATADIR=$WORKDIR/data
+HLDIR=$DATADIR/in/HL
+PARSERDIR=$DATADIR/out/parser
+LOGDIR=$DIR/LOGS
 
 cd $DIR
 TIMESTAMP=`date +'%s'`
@@ -12,4 +15,8 @@ NODE=/usr/local/n/versions/node/7.6.0/bin/node
 grep reques LOGS/* | perl -ne '/(S(?:X|\d){16})\s/ and print "$1\n"' | sort -u > $HLDIR/pbaccess
 $NODE runGetPiis.js 2>LOGS/getpiis.$TIMESTAMP
 $NODE getUnknownPiis.js $HLDIR PIIS/ > $UNKNOWN
-$NODE runGetHLPar.js $UNKNOWN > $HLDIR/HL.$TIMESTAMP 2> LOGS/log.$TIMESTAMP
+$NODE runGetHLPar.js $UNKNOWN > $HLDIR/HL.$TIMESTAMP 2> $LOGDIR/log.$TIMESTAMP
+
+cd ..
+./extractNewHL $HLDIR $PARSERDIR > $DATADIR/out/PENDING.$TIMESTAMP 2>$LOGDIR/make.$TIMESTAMP
+sh make.sh $DATADIR/out/PENDING.$TIMESTAMP 2>>$LOGDIR/make.$TIMESTAMP
