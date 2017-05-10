9781266188cbd1d273ed888bae654cf41537634d
Thy <thierry@ovh1>
2017-05-10 09:43:54 +0200

    Add explanation to the result

--- scripts/index/index-server/app/services/QueryHandlerHTML.scala
+++ scripts/index/index-server/app/services/QueryHandlerHTML.scala
@@ -5,11 +5,11 @@ import org.apache.lucene.analysis.Analyzer
 import org.apache.lucene.analysis.standard.StandardAnalyzer
 import org.apache.lucene.index._
 import org.apache.lucene.store.{Directory, FSDirectory}
-import org.apache.lucene.search.{TermQuery, IndexSearcher, ScoreDoc}
+import org.apache.lucene.search.{Query, TermQuery, IndexSearcher, ScoreDoc}
 import org.apache.lucene.queryparser.classic.QueryParser
 import org.apache.lucene.analysis.Analyzer
 import org.apache.lucene.document.Document
-
+import org.apache.lucene.search.Explanation
 import services.{P, Paper, PaperLookup}
 
 object QueryHandlerHTML {
@@ -25,22 +25,38 @@ object QueryHandlerHTML {
 <link rel="stylesheet" href="resources/page_files/articleSprite_1704R1.css" type="text/css">
 <link rel="stylesheet" href="resources/page_files/jquery-ui_1704R1.css" type="text/css">
 <link rel="stylesheet" href="resources/styles/cg.css" type="text/css">
+<script src="resources/js/utils.js"> </script>
 </head>"""
 
   val indexDir = "/home/thierry/tmp/index"
 
   def getDirectory (path: String) : Directory =
     FSDirectory open FileSystems.getDefault.getPath(path)
+  val dr = DirectoryReader open getDirectory (indexDir)
 
-  def getPaperElement (d: Document) = {
+  def getPaperElement (dd: Document, searcher: IndexSearcher, query: Query, docId: Int) = {
+    val d: Document = searcher.doc (docId)
     val pii = d get "pii"
+    val explanationDivId = s"sodad-explain-id-$pii"
     val maybePaper = PaperLookup (pii)
-    "<div class=\"sodad-list-paper\">\n" + 
-    s"<div class=${q}sodad-list-paper-pii$q>$pii</div>\n" +
+    "<div class=\"sodad-list-paper\">\n" +
+    s"<div class=${q}sodad-list-paper-pii$q>$pii\n" +
      (if (maybePaper.isDefined) {
        val p = maybePaper.get
        val titleClean = "\"".r.replaceAllIn (p.title, "")
-       s"<div class=${q}sodad-list-paper-title$q><a href=$q/paper?pii=$pii$q target=${q}_blank$q>$titleClean</a></div>\n" +
+       /*
+        Explanation of the weights
+        */
+       val explanation : Explanation = searcher.explain(query, docId)
+       s"<span class=${q}sodad-tooltip-class$q onclick=${q}flipVisibilityForId('$explanationDivId')$q style=${q}cursor: pointer;$q>" +
+       s"Explain<span class=${q}sodad-tooltiptext-class$q>Shows/Hides weighting scheme for this result item</span></span>\n" +
+       "</div>\n" +
+       s"<div class=${q}sodad-explanation-class$q id=${q}$explanationDivId$q style=${q}height: 0px; visibility: hidden;$q data-visible=${q}0$q>" +
+       explanation.toHtml() +
+       "</div>\n" +
+       s"<div class=${q}sodad-list-paper-title$q>\n" +
+       s"<a href=$q/paper?pii=$pii$q target=${q}_blank$q>$titleClean</a>\n" +
+       "</div>\n" +
        s"<div class=${q}sodad-list-paper-journal$q>${p.journal}</div>\n" +
        s"<div class=${q}sodad-list-paper-hls$q>${p.hl}</div>\n" +
        s"<div class=${q}sodad-list-submit-div$q>\n<form action=${q}/query$q>\n" +
@@ -53,40 +69,68 @@ object QueryHandlerHTML {
 
   def getHeader = header
 
-/*
-  def getEndScript (papers: Array[Option[P]]) : String = {
-    papers.foldLeft ("<script>\n") (
-      (acc, p) => p match {
-        case None => acc
-        case Some(p) => {
-          acc +
-          s"(function () {var e = document.getElementById(${q}hidden_${p.pii}$q)
-        }
-      }
-    ) + "</script>\n"
-  }
- */
 
   def apply (q: String, n: Int) : String = {
-    val dr = DirectoryReader open getDirectory (indexDir)
     try {
       val searcher = new IndexSearcher (dr)
       val parser = new QueryParser ("title", new StandardAnalyzer)
       val query = parser parse q
       val docs = searcher search (query, n)
-      /*
-      val papers : Array[Option[P]] = docs.scoreDocs.map (x => {
-        val d = searcher.doc (x.doc)
-        val pii = d get "pii"
-        PaperLookup (pii)
-      })
-       */
       docs.scoreDocs.foldLeft (s"<html>\n${getHeader}\n<body><div class=${q}sodad-list$q>\n")(
-        (acc, x) => acc + getPaperElement (searcher.doc (x.doc))) + 
+        (acc, x) => acc + getPaperElement (searcher.doc (x.doc), searcher, query, x.doc)) + 
       s"</body></html>\n"
     }
     finally {
-      dr.close ()
     }
   }
+
+  def escapeQuery (s : String) : String = {
+    var x = """\\""".r.replaceAllIn (s, "\\\\")
+    x = "\\+".r.replaceAllIn (x, "\\\\+")
+    x = "-".r.replaceAllIn (x, "\\\\-")
+    x = "!".r.replaceAllIn (x, "\\\\!")
+    x = "\\(".r.replaceAllIn (x, "\\\\(")
+    x = "\\)".r.replaceAllIn (x, "\\\\)")
+    x = ":".r.replaceAllIn (x, "\\\\:")
+    x = "\\^".r.replaceAllIn (x, "\\\\^")
+    x = "\\[".r.replaceAllIn (x, "\\\\[")
+    x = "]".r.replaceAllIn (x, "\\\\]")
+    x = "\\{".r.replaceAllIn (x, "\\\\{")
+    x = "}".r.replaceAllIn (x, "\\\\}")
+    x = "~".r.replaceAllIn (x, "\\\\~")
+    x = "\\*".r.replaceAllIn (x, "\\\\*")
+    x = "\\?".r.replaceAllIn (x, "\\\\?")
+    x
+  }
+
+  def buildSpecifDiv (title: String, hl: String) : String = {
+    "<div class=\"sodad-specif-div-class\">\n" + (
+      if (title != "")
+        s"<span class=${q}sodad-specif-lbl-class$q>Search on title:</span><span class=${q}sodad-specif-value-class$q>$title</span>" +
+        (if (hl != "") "<br/>\n" else "")
+      else "") + (
+      if (hl != "")
+        s"<span class=${q}sodad-specif-lbl-class$q>Search on highlights:</span><span class=${q}sodad-specif-value-class$q>$hl</span>"
+      else "") +
+    "</div>\n"
+  }
+
+  def applyLong (title: String, hl: String, n: Int) = {
+    try {
+      val searcher = new IndexSearcher (dr)
+      val parser = new QueryParser ("title", new StandardAnalyzer)
+      var fullQuery : String = ""
+      if (title != "") fullQuery = s"(${escapeQuery (title)})"
+      if (hl != "") fullQuery = s"$fullQuery hl:(${escapeQuery (hl)})"
+      Console println fullQuery
+      val query = parser parse fullQuery
+      val docs = searcher search (query, n)
+      docs.scoreDocs.foldLeft (s"<!DOCTYPE html>\n<html>\n${getHeader}\n<body>${buildSpecifDiv(title, hl)}<div class=${q}sodad-list$q>\n")(
+        (acc, x) => acc + getPaperElement (searcher.doc (x.doc), searcher, query, x.doc)) + 
+      s"</body></html>\n"
+    }
+    finally {
+    }
+  }
+
 }
