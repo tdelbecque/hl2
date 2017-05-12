15e196c5ea073c72fad9c2a2d36a24212079a8d5
Thy <thierry@ovh1>
2017-05-12 07:53:34 +0200

    use the cookies to get the default weight parameters

--- scripts/index/index-server/app/controllers/PaperController.scala
+++ scripts/index/index-server/app/controllers/PaperController.scala
@@ -8,8 +8,25 @@ import services.Paper
 
 @Singleton
 class PaperController @Inject() extends Controller {
-  def get (pii: String) = Action {
-    Paper.get (pii) match {
+  def get (pii: String) = Action { request =>
+    val searchOnTitle = request.cookies.get ("searchontitle") match {
+      case Some(Cookie(_,x,_,_,_,_,_)) => x.toBoolean
+      case None => true
+    }
+    val searchOnHL = request.cookies.get ("searchonhl") match {
+      case Some(Cookie(_,x,_,_,_,_,_)) => x.toBoolean
+      case None => true
+    }
+    val titleWeight = request.cookies.get ("titleweight") match {
+      case Some(Cookie(_,x,_,_,_,_,_)) => x.toDouble
+      case None => 0.5
+    }
+    val hlWeight = request.cookies.get ("hlweight") match {
+      case Some(Cookie(_,x,_,_,_,_,_)) => x.toDouble
+      case None => 0.5
+    }
+
+    Paper.get (pii, searchOnTitle, searchOnHL, titleWeight, hlWeight) match {
       case None => BadRequest ("Nothing for " + pii)
       case Some(result) => Ok(result).as ("text/html")
     }
