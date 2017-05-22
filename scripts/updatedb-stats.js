68835101e3f80a4cf038f39d019e30be1c042daf
Thy <thierry@ovh1>
2017-05-22 12:28:20 +0200

    Output to file only

--- scripts/updatedb-stats.js
+++ scripts/updatedb-stats.js
@@ -13,18 +13,30 @@ function isNoun (fields) {
 	(t === "NP") || (t ===  "NPS")
 }
 
+function isConcept (fields) {
+    return isNoun (fields) && fields [2].startsWith ("OmniConcept")
+}
+
 function isVerb (fields) {
     var t = fields [1]
-    return (t === "VB") || (t === "VBD") || (t === "VBG") || (t === "VBN") ||
-	(t === "VBP") || (t === "VBZ")
+    return (t === "VB") ||
+	(t === "VBD") ||
+	(t === "VBG") ||
+	(t === "VBN") ||
+	(t === "VBP") ||
+	(t === "VBZ")
 }
 
 function isVerbStrong (fields) {
     var t = fields [1]
     var l = fields [2]
-    var b = (t === "VB") || (t === "VBD") || (t === "VBG") || (t === "VBN") ||
+    var b = (t === "VBD") || (t === "VBN") ||
 	(t === "VBP") || (t === "VBZ")
     return b && (l !== "be") && (l !== "have")
+}
+
+function isVerbUnknown (fields) {
+    return isVerb (fields) && fields [4] === "unk"
 }
 
 class Process {
@@ -68,7 +80,6 @@ class Process {
 	this.looping = true
 	while (this.Q.length) {
 	    let e = this.Q.shift ()
-	    //console.error (`${e.pii}\t${e.hlno}\t${e.nTokens}\t${e.nNouns}\t${e.nVerbs}\t${e.nVerbsStrong}\t${e.tokens.join (" ")}`)
 	    let k = `${e.pii} ${e.hlno}`
 	    if (! this.dic.has (k)) {
 		let hl = e.tokens.join (" ").replace (/\'/g, "''")
@@ -85,7 +96,33 @@ class Process {
 	if (this.canClose) this.client.end ()	
 	this.looping = false
     }
-    
+
+    async loopPrint () {
+	this.looping = true
+	while (this.Q.length) {
+	    let e = this.Q.shift ()
+	    let hl = e.tokens.join (" ").replace (/\'/g, "''")
+	    let q = []
+	    q.push (e.pii)
+	    q.push (e.hlno)
+	    q.push (e.nTokens)
+	    q.push (e.nSub)
+	    q.push (e.nPred)
+	    q.push (e.nObj)
+	    q.push (e.nUnk)
+	    q.push (e.nNouns)
+	    q.push (e.nConcepts)
+	    q.push (Object.keys (e.concepts).length)
+	    q.push (e.nVerbs)
+	    q.push (e.nVerbsStrong)
+	    q.push (e.nVerbsUnknown)
+	    q.push (hl)
+	    console.log (q.join ("\t"))
+	}
+	if (this.canClose) this.client.end ()
+	this.looping = false
+    }
+
     async addLinesAsync (xs, file) {
 	var Q = this.Q
 	var S = this.S [file]
@@ -102,12 +139,16 @@ class Process {
 		S.nTokens = 0
 		S.nVerbs = 0
 		S.nVerbsStrong = 0
+		S.nVerbsUnknown = 0
 		S.nNouns = 0
+		S.nConcepts = 0
 		S.nSub = 0
 		S.nPred = 0
 		S.nObj = 0
 		S.nUnk = 0
+		S.concepts = {}
 		S.tokens = []
+		S.lemmas = []
 		return
 	    }
 	    if (x.match (/^<\/HL/)) {
@@ -123,15 +164,23 @@ class Process {
 	    if (fields.length != 5) return
 	    S.tokens.push (fields [0])
 	    S.nTokens ++
-	    if (isNoun (fields)) S.nNouns ++
+	    if (isNoun (fields)) {
+		S.nNouns ++
+		S.lemmas.push (fields [2])
+	    }
+	    if (isConcept (fields)) {
+		S.nConcepts ++
+		S.concepts [fields [2]] = 1
+	    }
 	    if (isVerb (fields)) S.nVerbs ++
+	    if (isVerbUnknown (fields)) S.nVerbsUnknown ++
 	    if (isVerbStrong (fields)) S.nVerbsStrong ++
 	    if (fields [4] == 'sub') S.nSub ++
 	    else if (fields [4] == 'pred') S.nPred ++
 	    else if (fields [4] == 'obj') S.nObj ++
 	    else S.nUnk ++
 	})
-	if (Q.length && ! this.looping) this.loop ().catch (err => U.croak (err))
+	if (Q.length && ! this.looping) this.loopPrint ().catch (err => U.croak (err))
      }
     
     close () {
@@ -142,9 +191,11 @@ class Process {
 }
 
 async function main () {
+    var header = "pii\thlno\tntokens\tnsub\tnpred\tnobj\tnunk\tnnouns\tnconcepts\tndistinctconc\tnverbs\tnverbsstrong\tnverbsunknown\thl"
+    console.log (header)
     const p = new Process ()
     try {
-	await p.init ()
+	//await p.init ()
 	await p.processDir ()
     }
     catch (err) {
