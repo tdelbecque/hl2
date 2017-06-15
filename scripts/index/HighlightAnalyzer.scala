18ea9a766bc925e86981b581333195d68d975259
Thy <thierry@ovh1>
2017-06-15 17:20:41 +0200

    Add lowercase and pos filter

--- scripts/index/HighlightAnalyzer.scala
+++ scripts/index/HighlightAnalyzer.scala
@@ -1,13 +1,19 @@
 //package com.sodad.lucene.utils
 
-import org.apache.lucene.analysis.Analyzer
+import org.apache.lucene.analysis.{Analyzer, LowerCaseFilter}
 import org.apache.lucene.analysis.Analyzer._
 
 class HighlightAnalyzer extends Analyzer {
   @Override
   protected def createComponents(fieldName: String) : TokenStreamComponents = {
     val t = new HighlightTokenizer
-    new TokenStreamComponents(t, new HighlightTokenStreamFilter (t))
+
+    new TokenStreamComponents(
+      t,
+      new LowerCaseFilter (
+        new HighlightTokenStreamFilter (
+          new PartOfSpeechTokenStreamFilter (t))))
+
   }
 }
 
