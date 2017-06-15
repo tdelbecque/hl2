b2805cc6f5ca61a52930444f635cd35ed30fec92
Thy <thierry@ovh1>
2017-06-15 17:33:09 +0200

    moved

---
+++ scripts/analysis/patch.pl
@@ -0,0 +1,44 @@
+use strict;
+use diagnostics;
+
+my %D;
+
+my $patchFile = shift @ARGV;
+$/="</PAPER>";
+
+open PATCHFILE, "<$patchFile" or die "$!";
+
+while (<PATCHFILE>) {
+    my ($pii) = /PII\s*=\s*"(.+?)"/;
+    last unless $pii and m!</PAPER>!;
+    $D {$pii} = my $data = [];
+    my @HL = m!(<HL.+?/HL>)!sg;
+    for (@HL) {
+	my ($r) = /<HL\s+rank\s*=\s*"(.+?)"/;
+	$data -> [$r - 1] = $_;
+    }
+}
+
+close PATCHFILE;
+
+while (<>) {
+    my ($pii) = /PII\s*=\s*"(.+?)"/;
+    last unless $pii;
+    next unless my $data = $D {$pii};
+    my $i = 0;
+    print "<PAPER PII=\"$pii\">\n";
+    my @HL = m!(<HL.+?/HL>)!sg;
+    for (@HL) {
+	my $hl = $data -> [$i];
+	if ($hl) {
+	    print "$hl\n";
+	} else {
+	    print "$_\n";
+	}
+	$i ++;
+    }
+    print "</PAPER>\n";
+}
+
+
+
