fb86f4c4e86dcab7c74f3bfe9138aa83fca34ee3
Thy <thierry@ovh1>
2017-06-22 17:47:55 +0200

    Accepts save directory in the command line parameters

--- scripts/network/runGetHLPar.js
+++ scripts/network/runGetHLPar.js
@@ -1,13 +1,11 @@
-var PII_File = process.argv [2]
-
+const PII_File = process.argv [2]
+const pageSaveDir = process.argv [3]
 var nAgents = 8
 
-var undef
-
-if (PII_File === undef) {
+if (PII_File === undefined) {
     var fn = __filename.replace (process.cwd(), '.')
     process.stderr.write ("\nusage: node " + fn + 
-			  " argument\n")
+			  " argument [pageSaveDir]\n")
     process.stderr.write ("where argument is either some list of pii, or a path to a pii file\n")
     process.stderr.write ("For example:\n")
     process.stderr.write ("\tnode " + fn + ' "S0961953411005964;S0379711212000185"\nor\n')
@@ -18,7 +16,7 @@ if (PII_File === undef) {
 var p = require ('./gethl')
 var fs = require ('fs')
 
-var f = new p ()
+var f = new p (pageSaveDir)
 if (! f.testValid().isValid)
     throw "ERR INITIALIZE"
 
@@ -32,7 +30,7 @@ function whenFinished (x) {
 
 for (var i = 0; i < nAgents; i ++) 
     (function (piis) {
-	var g = new p ();
+	var g = new p (pageSaveDir);
 	g.piis = piis.filter (function (x, j) {return j % nAgents === i})
 	if (g.piis.length > 0)
 	    g.get (whenFinished)
