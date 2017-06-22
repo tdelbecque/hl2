abc7e5f45e8969e773e86879023bd5a537dae260
Thy <thierry@ovh1>
2017-06-22 17:47:08 +0200

    Accepts the save directoory in the command line arguments

--- scripts/network/runGetHL.js
+++ scripts/network/runGetHL.js
@@ -1,10 +1,10 @@
-var PII_File = process.argv [2]
-var undef
-
-if (PII_File === undef) {
+const PII_File = process.argv [2]
+const pageSaveDir = process.argv [3]
+      
+if (PII_File === undefined) {
     var fn = __filename.replace (process.cwd(), '.')
     process.stderr.write ("\nusage: node " + fn + 
-			  " argument\n")
+			  " argument [pageSaveDir]\n")
     process.stderr.write ("where argument is either some list of pii, or a path to a pii file\n")
     process.stderr.write ("For example:\n")
     process.stderr.write ("\tnode " + fn + ' "S0961953411005964;S0379711212000185"\nor\n')
@@ -15,7 +15,7 @@ if (PII_File === undef) {
 var p = require ('./gethl')
 var fs = require ('fs')
 
-var f = new p ()
+var f = new p (pageSaveDir)
 if (! f.testValid().isValid)
     throw "ERR INITIALIZE"
 
