8853f29e3b925838a3c24108dc6d557d0af1ae7c
Thy <thierry@ovh1>
2017-07-13 18:43:52 +0200

    tag with <THY_>

--- scripts/NLM/extract-table.pl
+++ scripts/NLM/extract-table.pl
@@ -4,9 +4,10 @@ use diagnostics;
 $" = "\n";
 while (<>) {
     next unless /^INSERT/;
-    s/\(/<B>/;
-    s/\),\(/<A><B>/g;
-    my @tuples = /<B>(.+?)<A>/g;
+    s/'\),\((\d\d\d)/'<THY_A><THY_B>$1/g;
+    s/VALUES \(/<THY_B>/;
+    s/\);$/<THY_A>/;
+    my @tuples = /<THY_B>(.+?)<THY_A>/g;
     print "@tuples\n";
 }
 
