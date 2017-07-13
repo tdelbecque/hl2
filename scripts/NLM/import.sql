b7bf8b675e0a7b5a2eabf50cf3e7e93d3f184f62
Thy <thierry@ovh1>
2017-07-13 18:43:08 +0200

    more imports

--- scripts/NLM/import.sql
+++ scripts/NLM/import.sql
@@ -35,8 +35,25 @@ create table pmids (
        eid varchar(19));
 */
 
+/*
 create table predications as (
        select pii, B.* from pmids A join semmed_predications B on A.pmid = B.pmid);
        
 create index predications_idx on predications (pii);
 
+*/
+
+drop table if exists semmed_sentences;
+create table semmed_sentences (
+       SENTENCE_ID int NOT NULL,
+       PMID varchar(20) DEFAULT NULL,
+       type char(2),
+       number int,
+       sentence text);
+       
+create table sentences as (
+       select pii, B.* from pmids A join semmed_sentences B on A.pmid = B.pmid);
+       
+
+create index sentences_idx on sentences (pii);
+create index sentences_sent_idx on sentences (sentence_id);
